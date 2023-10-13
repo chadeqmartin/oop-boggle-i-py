ed359d5834686559b960b933ee814dcafabeb898
chadeqmartin <chadeqmartin@gmail.com>
2023-10-12 21:46:09 -0400

    updated with working solution

--- solution.py
+++ solution.py
@@ -1,4 +1,5 @@
 import string
+from itertools import repeat
 import random
 
 class Dice:
@@ -8,22 +9,35 @@ class Dice:
             self._dice.append(random.choice(string.ascii_letters).upper())
 
 class BoggleBoard:
-    
-    def __init__(self, size=4):
-        self._size = size
+
+    def __init__(self, row_size=4, board_length=4):
+        self._row_size = row_size
+        self._board_length = board_length
         self._board = []
 
+        for i in range(self._board_length):
+            row = list(repeat("_", self._row_size))
+            self._board.append(row)
+
     def __str__(self):
-        if len(self._board) == 0:
-            output = ''
-            for i in range(self._size):
-                output += '----\n'
-            return output
-        else:
-            for i in range(4):
-                for j in range(4):
-                    output += self._board[i][j]
-            return output
-        
+        output = ''
+        for i in range(self._board_length):
+            for j in range(self._row_size):
+                output += self._board[i][j]
+            output += "\n"
+        return output
+    
+    
+    def shake(self):
+        for i in range(self._board_length):
+            for j in range(self._row_size):
+                each_dice = Dice()
+                dice_roll = random.choice(each_dice._dice)
+                self._board[i][j] = dice_roll + "  "
+                if self._board[i][j] == "Q  ":
+                    self._board[i][j] = "Qu "
+    
 new_bog = BoggleBoard()
+print(new_bog)
+new_bog.shake()
 print(new_bog)
